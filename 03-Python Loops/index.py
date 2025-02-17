dd53c973a3ee0e8271b200a94973c5451bb688d4
Maykal Tenev <michaeltenev@gmail.com>
2025-02-18 00:22:25 +0100

    03_python_loops

---
+++ 03-Python Loops/index.py
@@ -0,0 +1,14 @@
+mylist = [1, 2, 3, 4, 56]
+for num in mylist:
+    if num % 2 == 0:
+        print(num)
+    else:
+        print(f'Odd number: {num}')
+
+x = 0
+while x < 5:
+    print(f'The current value of x is {x}')
+ #   x = x + 1
+    x += 1
+else:
+    print("X IS NOT LESS THAN 5")
